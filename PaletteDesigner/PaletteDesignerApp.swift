a1b08cb17aa8c997736fd9ee7463ced847eb88c1
Russell Gordon <rgordon@lcs.on.ca>
2024-09-08 09:28:49 -0400

    Initial Commit

---
+++ PaletteDesigner/PaletteDesignerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaletteDesignerApp.swift
+//  PaletteDesigner
+//
+//  Created by Russell Gordon on 2024-09-08.
+//
+
+import SwiftUI
+
+@main
+struct PaletteDesignerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
