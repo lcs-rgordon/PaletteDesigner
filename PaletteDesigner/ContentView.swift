c9d629bf2b26e2c820ace0a2cd8e1ec27d47d4fa
Russell Gordon <rgordon@lcs.on.ca>
2024-09-08 09:48:29 -0400

    Can now add colors, by hex, to a palette.

--- PaletteDesigner/ContentView.swift
+++ PaletteDesigner/ContentView.swift
@@ -5,17 +5,55 @@
 //  Created by Russell Gordon on 2024-09-08.
 //
 
+import NVMColor
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    // MARK: Stored properties
+    @State private var providedColorInHex: String = "AFAFAF"
+    @State private var providedColors: [Color] = []
+
+    // MARK: Computed properties
+    var providedColor: Color {
+        guard let providedColor = Color(hex: providedColorInHex) else {
+            return .black
         }
-        .padding()
+        return providedColor
+    }
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                
+                TextField("Type a color in hexadecimal:", text: $providedColorInHex)
+                
+                Rectangle()
+                    .frame(width: 200, height: 100)
+                    .foregroundStyle(providedColor)
+                
+                Button {
+                    providedColors.append(providedColor)
+                } label: {
+                    Text("Add")
+                }
+                
+                VStack(spacing: 0) {
+                    ForEach(providedColors, id: \.self) { currentColor in
+                        Rectangle()
+                            .frame(width: 200, height: 100)
+                            .foregroundStyle(currentColor)
+                    }
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .background {
+                Color.white
+            }
+
+        }
     }
 }
 
